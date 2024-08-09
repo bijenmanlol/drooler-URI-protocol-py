462b01d99d7a465711443362695eefefca816284
drooler <85669046+bijenmanlol@users.noreply.github.com>
2024-08-09 16:07:25 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -4,5 +4,5 @@ cd drooler
 curl "https://raw.githubusercontent.com/bijenmanlol/drooler-URI-protocol-py/main/drooler.bat" --output drooler.bat
 curl "https://raw.githubusercontent.com/bijenmanlol/drooler-URI-protocol-py/main/main.py" --output main.py
 curl "https://raw.githubusercontent.com/bijenmanlol/drooler-URI-protocol-py/main/data.json" --output data.json
-msg "%username%" "Drooler-URI-protocol-py has been installed"
 py main.py
+msg "%username%" "Drooler-URI-protocol-py has been installed"
